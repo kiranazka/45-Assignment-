a8a2f189bb2c6cc735ed861069ff4e3c80573ce4
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-04-01 13:29:33 -0700

    Q:19
    Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
    
    Signed-off-by: kiranazka <160616395+kiranazka@users.noreply.github.com>

--- main.ts
+++ main.ts
@@ -1,18 +1,2 @@
-let countiesToVist: string[] = ["China","Pakistan","America","Bahrain","Sweden"];
-console.log("Orginal order:",countiesToVist);
-// print the array in Alphabetical order without modifying the Actual Array list
-console.log("Alphabetical order :",[...countiesToVist].sort());
-// print the array  still in Orginal order   modifying the Actual Array list
-console.log("still in Orginal order:",countiesToVist);
-// print the array Reverse order   modifying the Actual Array  reverse list
-console.log("Reverse order:",[...countiesToVist].reverse());
-// print the array  still in Orginal order   modifying the Actual Array list
-console.log("still in Orginal order:",countiesToVist);
-// print the array   Orginal Array reversed order   modifying the Actual  reverse Array list
-console.log ("orginal Array reversed:",countiesToVist.reverse());
-// print the array  back to Orginal order   modifying the Actual Array reverse list
-console.log("back to Orginal order:",countiesToVist.reverse());
-// print the sorted in Alphabetical order   modifying the Actual sort 
-console.log("sorted in Alphabetical order:",countiesToVist.sort());
-// print the array  Orginal  reversed again  modifying the Actual Array  revrse list
-console.log ("orginal Array reversed again:",countiesToVist.reverse());
+let guestlist = ["Azka","Ahmad","Kamran","Sumara"];
+guestlist.forEach(oneguest => console.log(`Salam ${oneguest}, 'would you like to invite to dinner with me?'`));
