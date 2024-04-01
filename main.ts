4cf8b1e5e40244098e783788851292fd58a52d58
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-04-01 12:45:02 -0700

    Q 2
    Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
    
    Signed-off-by: kiranazka <160616395+kiranazka@users.noreply.github.com>

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-console.log ('Helo World');
+ let favoritecolor= ("blue");
+ console.log(favoritecolor)
+ favoritecolor = "green";
+ console.log(favoritecolor);
+ favoritecolor = "black"
+ console.log(favoritecolor);
+ favoritecolor = "white"
+ console.log(favoritecolor);
