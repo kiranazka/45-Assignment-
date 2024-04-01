6430f47fb7f0958c9da4ad4a2699b9e547c159bc
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-04-01 13:26:57 -0700

    Q:16
    More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
    
    Signed-off-by: kiranazka <160616395+kiranazka@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-var guestlist = ["Ayesha", "Zarwa", "Zumra", "Hafsa", "Azka"];
-var dontCome = guestlist[0];
-console.log(dontCome, "I cannot come to the Invitation");
-guestlist.splice(0, 0, "Ahmad");
-guestlist.forEach(function (guest) { return console.log("salam ".concat(guest, ",would You Like to Dinner with me")); });
+//Creating guestlist Array
+var guestlist = ["Kinza", "Azka", "Nida", "Iqra", "Afnan"];
+// Making variable for those guest who cant come
+var dontCome = guestlist[1];
+//print  the name of guest dont come
+console.log(dontCome, "Nai a Sakty hai");
+// Add or Remove from Guest List Array
+guestlist.splice(1, 2, "Burhan");
+// print Massge  
+console.log("Good News ! We have found a Bigger Table For Dinner");
+// Adding the new guest at starting index Array
+guestlist.unshift("Ali");
+// Adding a new guest at ending index Array
+guestlist.push("Zain");
+// Adding one new guest at middal index of Array
+var middalIndex = Math.floor(guestlist.length / 2);
+// Adding a new guest to middleindex of Array
+guestlist.splice(middalIndex, 0, "Osama");
+console.log("update list of our Guest");
+guestlist.forEach(function (oneguest) { return console.log("Salam ".concat(oneguest, ",Would you like dinner with me")); });
--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
-let guestlist = ["Ayesha","Zarwa","Zumra","Hafsa","Azka"];
-let dontCome = guestlist[0];
-console.log(dontCome,"I cannot come to the Invitation");
-guestlist.splice(0,0,"Ahmad");
-guestlist.forEach(guest => console.log(`salam ${guest},would You Like to Dinner with me`));
+//Creating guestlist Array
+let guestlist = ["Kinza","Azka","Nida","Iqra","Afnan"];
+// Making variable for those guest who cant come
+let dontCome = guestlist[1];
+//print  the name of guest dont come
+console.log(dontCome,"Nai a Sakty hai");
+ // Add or Remove from Guest List Array
+guestlist.splice(1,2, "Burhan");
+// print Massge  
+console.log("Good News ! We have found a Bigger Table For Dinner");
+// Adding the new guest at starting index Array
+guestlist.unshift("Ali");
+// Adding a new guest at ending index Array
+guestlist.push("Zain");
+// Adding one new guest at middal index of Array
+let middalIndex:number =Math.floor (guestlist.length/2);
+// Adding a new guest to middleindex of Array
+guestlist.splice(middalIndex,0,"Osama");
+console.log("update list of our Guest");
+guestlist.forEach(oneguest => console.log(`Salam ${oneguest},Would you like dinner with me`));
