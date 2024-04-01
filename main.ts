cbe3892774b299b85fbffb4798693489acab77ae
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-04-01 13:15:53 -0700

    Q:9
    Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
    
    Signed-off-by: kiranazka <160616395+kiranazka@users.noreply.github.com>

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-// print Addition
-console.log(4+4);
-// print subtraction 
-console.log(10-2);
-// print multiplication 
-console.log(4*2);
-// print Division 
-console.log(16/2);
+let favNumber = 15;
+let massage = "my favNumber is:";
+console.log(massage,favNumber);
