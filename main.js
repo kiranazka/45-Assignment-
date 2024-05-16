4d96a19d081c3b1dffc9e716014cb7d277e4e42a
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-05-16 14:42:15 -0700

    Q:21
    45 Assignment
    
    
    Signed-off-by: kiranazka <160616395+kiranazka@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,27 +1,9 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
+var course = {
+    Name: "TypeScript && JavaScrit",
+    loction: "GovernerSindh",
+    Duration: "12 months",
+    onSiteStudent: 50000,
+    onSitefaculty: 100,
+    onSiteStaff: 100,
 };
-var countiesToVist = ["China", "Pakistan", "America", "Bahrain", "Sweden"];
-console.log("Orginal order:", countiesToVist);
-// print the array in Alphabetical order without modifying the Actual Array list
-console.log("Alphabetical order :", __spreadArray([], countiesToVist, true).sort());
-// print the array  still in Orginal order   modifying the Actual Array list
-console.log("still in Orginal order:", countiesToVist);
-// print the array Reverse order   modifying the Actual Array  reverse list
-console.log("Reverse order:", __spreadArray([], countiesToVist, true).reverse());
-// print the array  still in Orginal order   modifying the Actual Array list
-console.log("still in Orginal order:", countiesToVist);
-// print the array   Orginal Array reversed order   modifying the Actual  reverse Array list
-console.log("orginal Array reversed:", countiesToVist.reverse());
-// print the array  back to Orginal order   modifying the Actual Array reverse list
-console.log("back to Orginal order:", countiesToVist.reverse());
-// print the sorted in Alphabetical order   modifying the Actual sort 
-console.log("sorted in Alphabetical order:", countiesToVist.sort());
-// print the array  Orginal  reversed again  modifying the Actual Array  revrse list
-console.log("orginal Array reversed again:", countiesToVist.reverse());
+console.log(course);
--- main.ts
+++ main.ts
@@ -1,2 +1,19 @@
-let guestlist = ["Azka","Ahmad","Kamran","Sumara"];
-guestlist.forEach(oneguest => console.log(`Salam ${oneguest}, 'would you like to invite to dinner with me?'`));
+// Object  interface
+interface itCourse {
+     Name: string;
+     loction: string;
+     Duration: string;
+     onSiteStudent:number;
+     onSitefaculty:number;
+     onSiteStaff:number;
+     
+}
+let course:itCourse = {
+    Name: "TypeScript && JavaScrit",
+    loction: "GovernerSindh",
+    Duration: "12 months",
+    onSiteStudent:50000,
+    onSitefaculty:100,
+    onSiteStaff:100,
+}
+console.log(course);
