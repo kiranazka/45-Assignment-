a07b126048069370b1d3db36090bdb923ca568ec
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-05-16 14:44:16 -0700

    Q: 23
    45 assignment
    
    
    Signed-off-by: kiranazka <160616395+kiranazka@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,33 @@
+// create 2 variable 
+var five = 5;
+var ten = 10;
+// Test 1
+console.log("\n Test 1: Five is the equal to 5?\n");
+console.log(five === 5);
+// Test 2
+console.log("\n Test 2: ten is the equal to 10?\n");
+console.log(ten == 10);
+// Test 3
+console.log("\n Test 3: five is the not equal to 10?\n");
+console.log(five != 10);
+// Test :4
+console.log("\n Test 4: 10 is greater then 5?\n");
+console.log(ten > 5);
+// Test 5
+console.log("\n Test 5: 5 is smaller then 10?\n");
+console.log(five < 10);
+// Test 6
+console.log("\n Test 6: 10 is smaller then 5?\n");
+console.log(ten < 5);
+// Test 7
+console.log("\n Test 7: 5 is greater then 10?\n");
+console.log(five > 10);
+// Test 8
+console.log("\n Test 8: ten is not equal to 10?\n");
+console.log(ten != 10);
+// Test 9
+console.log("\n Test 9: ten is less then to 10?\n");
+console.log(ten < 10);
+// Test 10
+console.log("\n Test 10: ten is greater then to 10?\n");
+console.log(ten > 10);
