98cd36cf12fad0f00916e03e07884cc3ee96640b
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-05-20 18:58:47 +0500

    Add files via upload
    Signed-off-by: kiranazka <160616395+kiranazka@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-//Create Array
-var error = ["A", "B", "C", "D", "E"];
-// Production Error ! by Accessing invalid index of Array
-console.log(error[6]);
-// Create Array of object
-console.log(error);
-// create 2 Array value  object
-console.log(error[2]);
-// create 5 Array Object
-console.log(error[5]);
+// Define variable
+var alianColor = "Black";
+// if statement
+if (alianColor === "Black") {
+    console.log("The player just earned 5 points.");
+}
+if (alianColor === "white") {
+    console.log("The player just earned 10 points.");
+}
--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
-//Create Array
-let errorArray : string[] = ["A","B","C","D","E"];
-// Production Error ! by Accessing invalid index of Array
-console.log(errorArray[6]);
-// Create Array of object
-console.log(errorArray);
-// create 2 Array value  object
-console.log(errorArray[2]);
-// create 5 Array Object
-console.log(errorArray[4]);
 
+// Define variable
+let alianColor = "Black";
+// if statement
+if (alianColor === "Black"){
+    console.log("\nThe player just earned 5 points./n");
+}
+if ( alianColor === "white"){
+    console.log( "\nThe player just earned 10 points.\n");
+}
 
