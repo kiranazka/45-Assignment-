5abf3fea90dfeacbd9f71226b6f77861570ac4a9
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-05-21 03:26:29 +0500

    Q. 30
    Signed-off-by: kiranazka <160616395+kiranazka@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
-"use strict";
-// creating Array
-let fruits = ["Banana", "Mango", "Orange", "Peach", "Apple"];
-if (fruits.includes("Mango")) {
-    console.log("Mango is My favourite fruit");
-}
-if (fruits.includes("Banana")) {
-    console.log("I like Banana");
-}
-if (fruits.includes("Orange")) {
-    console.log("Orange is My favourite fruit && So delicious");
-}
-if (fruits.includes("Peach")) {
-    console.log("Peach is My favourite fruit");
-}
-if (fruits.includes("Apple")) {
-    console.log("Apple is My favourite && healthy fruit");
-}
-if (fruits.includes("Kiwi")) {
-    console.log("Kiwi is My favourite fruit");
-}
-if (fruits.includes("Grapes")) {
-    console.log("Grapes is My favourite fruit");
-}
+// create admin Array
+var userName = ["Iqra", "Afnan", "Ahmad", "Kamran", "Admin"];
+// using foreach loop
+userName.forEach(function (oneUser) {
+    if (oneUser === "Admin") {
+        console.log("Hello ".concat(oneUser, ", Would you like to see a status report? "));
+    }
+    else {
+        console.log("Hello ".concat(oneUser, ", Thank you for logging in again"));
+    }
+});
--- main.ts
+++ main.ts
@@ -1,23 +1,12 @@
-// creating Array
-let fruits = ["Banana","Mango","Orange","Peach","Apple"]
-if(fruits.includes("Mango")){
-    console.log("Mango is My favourite fruit.");
-}
-if(fruits.includes("Banana")){
-    console.log("I like Banana");
-}
-if(fruits.includes("Orange")){
-    console.log("Orange is My favourite fruit && So delicious.");
-}
-if(fruits.includes("Peach")){
-    console.log("Peach is My favourite fruit.");
-}
-if(fruits.includes("Apple")){
-    console.log("Apple is My favourite && healthy fruit.");
-}
-if(fruits.includes("Kiwi")){
-    console.log("Kiwi is My favourite fruit.");
-}
-if(fruits.includes("Grapes")){
-    console.log("Grapes is My favourite fruit.");
-}
+// create admin Array
+let userName = ["Iqra","Afnan","Ahmad","Kamran","Admin"];
+ // using foreach loop
+ userName.forEach(oneUser => {
+    if(oneUser === "Admin"){
+        console.log(`Hello ${oneUser}, Would you like to see a status report? `);
+    }
+     else{
+        console.log(`Hello ${oneUser}, Thank you for logging in again`);
+     }
+     }
+    )
